f4a4b38d5678c766fc8055a58c751af57403cfb2
Gerben Pothoff <gerben@302.nl>
2021-11-26 13:19:34 +0100

    added local dev server (with ssl) for easy development register plugin to ef2go

--- src/plugin-buienradar.js
+++ src/plugin-buienradar.js
@@ -1,3 +1,7 @@
+const plugin = {
+    name: 'buienradar-paraplu'
+};
+
 let head = document.getElementsByTagName('head')[0];
 let jqueryScript = document.createElement('script');
 
@@ -5,15 +9,20 @@ jqueryScript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.mi
 
 head.appendChild(jqueryScript);
 
-$(document).ready(function () {
-    let plugin = $("#plugin-buienradar-paraplu");
+function launchPlugin() {
 
-    $.getJSON("https://data.buienradar.nl/2.0/feed/json", function( data ) {
+    console.log('mounting plugin', plugin.name);
+
+    let element = $("#plugin-buienradar-paraplu");
+
+    $.getJSON("https://data.buienradar.nl/2.0/feed/json", function (data) {
         let norain = '😎';
         let rain = '☔';
 
         let indicator = (data.forecast.fivedayforecast[0].mmRainMax > 5) ? rain : norain;
 
-        plugin.append("<h1>Morgen: " + indicator + "</h1>");
+        element.append("<h1>Morgen: " + indicator + "</h1>");
     });
-});
+}
+
+window.registerPlugin({ plugin }, launchPlugin);
